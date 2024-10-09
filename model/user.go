27c2ea724ed15721dd10803e07831743734e99bd
BerkWiro <berkcicekler7@gmail.com>
2024-10-09 18:35:33 +0300

    user model validator added

--- model/user.go
+++ model/user.go
@@ -2,8 +2,8 @@ package model
 
 type User struct {
 	ID      string `json:"id,omitempty" bson:"_id"`
-	UserName    string `json:"userName,omitempty" bson:"userName"`
+	UserName    string `json:"userName,omitempty" bson:"userName" validate:"required,min=5,max=25"`
 	Password    string `json:"password,omitempty" bson:"password"`
-	Email       string `json:"email,omitempty" bson:"email"`
+	Email       string `json:"email,omitempty" bson:"email" validate:"required,email"`
 	PhoneNumber string `json:"phoneNumber,omitempty" bson:"phoneNumber"`
 }
